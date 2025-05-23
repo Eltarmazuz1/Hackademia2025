ce5a14007f0030d4f963c4d75894192f4907b53e
MaiShiffman1 <maishiffman@gmail.com>
2025-05-23 06:06:30 +0300

    added the frontend function

---
+++ src/pages/ChatWithYourMate.tsx
@@ -0,0 +1,15 @@
+import { Box, TextField, IconButton } from "@mui/material";
+import MessageBuble from '@/components/ui/MessageBuble';
+import { SendIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import MessageChat from "@/components/ui/MessageChat";
+
+
+const ChatWithYourMate = () => {
+
+  return (
+    <MessageChat/>
+  );
+};
+
+export default ChatWithYourMate;
