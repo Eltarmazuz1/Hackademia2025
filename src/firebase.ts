263d2434de8b285dd7dd1f3ceede47edd04cbe44
Eltarmazuz1 <140516734+Eltarmazuz1@users.noreply.github.com>
2025-06-03 22:12:50 +0300

    Update

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,17 +4,16 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";  // <--- import storage
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_P7z1ZDEdGBmYSVCje0dyzhUUVp_lsoo",
-  authDomain: "hackademia-7b062.firebaseapp.com",
-  projectId: "hackademia-7b062",
-  storageBucket: "hackademia-7b062.appspot.com",  // <--- fix bucket URL
-  messagingSenderId: "356769156079",
-  appId: "1:356769156079:web:a5e63e4010ce01e8bb441a",
-  measurementId: "G-ENRJ1BG1TH"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);  // <--- export storage
+export const storage = getStorage(app); 
